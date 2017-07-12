e66afc6cebd2ea32423db3c32569c2bb38c1d839
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-07-13 00:05:16 +0900

    Added zip_to_geo

---
+++ zip_to_geo.py
@@ -0,0 +1,104 @@
+'''
+Created on Jul 12, 2017
+
+@author: akira
+'''
+
+import logging, logging.handlers
+import argparse
+import time
+import sys
+
+import requests
+
+MAX_TRY = 5
+TIMEOUT = 10
+INTERVAL = 5
+GOOGLE_API_KEY = "YOUR API KEY"
+
+logger = logging.getLogger(__name__)
+
+def rest_request(url, user=None, password=None):
+    status = None
+    tries = 0
+    while not status and tries < MAX_TRY:
+        try:
+            creds = (user, password) if user and password else None
+            resp = requests.get(url, verify=True, auth=creds, timeout=TIMEOUT)
+            status = resp.status_code
+        except requests.exceptions.ConnectionError as err:
+            logger.warning("rest_request: Could not reach '%s': %s. Trying again in %s seconds", url, err, INTERVAL)
+            time.sleep(INTERVAL)
+        except requests.exceptions.Timeout as err:
+            logger.warning("rest_request: Request to '%s' timed out after %d seconds: %s. Trying again.",
+                        url, TIMEOUT, err)
+        finally:
+            tries += 1
+            if tries >= MAX_TRY:
+                logger.error("rest_request: Could not reach '%s': %s", url, err)
+
+    if status == 200:
+        return resp
+    elif status != 200 and resp:
+        logger.error("rest_request: '%s' returned status code %s: %s", url, status, resp.text)
+        return None
+    else:
+        return None
+
+def logging_setup(args):
+    log_root = logging.getLogger('')
+    if args.verbose > 1:
+        log_root.setLevel(logging.DEBUG)
+    elif args.verbose == 1:
+        log_root.setLevel(logging.INFO)
+    
+    log_format = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(log_format)
+    log_root.addHandler(log_handler)
+
+def main(argv):
+    parser = argparse.ArgumentParser(description="Get latitude & longitude from zipcode")
+    parser.add_argument("-v", "--verbose", action="count", default=0, dest="verbose",
+                        help="Enable verbose output (specify multiple times to increase level)")
+    parser.add_argument("-z", "--zipcode", action="store", metavar="STRING", dest="zc", required=True,
+                        help="Zipcode to be queried")
+
+    args = parser.parse_args(argv)
+ 
+    # Logging setup
+    logging_setup(args)
+    log_main = logging.getLogger('main')
+    
+    if args.zc:
+        zc = args.zc
+    
+    url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode={}".format(zc)
+
+    r = rest_request(url)
+    if not r:
+        log_main("request failed for zipcode")
+        return 1
+    
+    data = r.json()
+    
+    addr1 = data['results'][0]['address1']
+    addr2 = data['results'][0]['address2']
+    addr3 = data['results'][0]['address3']
+    
+    log_main.info("fetched %s, %s, %s", addr1, addr2, addr3)
+    
+    url_ggl = "https://maps.googleapis.com/maps/api/geocode/json?address={0}{1}{2}&key={3}".format(addr1, addr2, addr3, GOOGLE_API_KEY)    
+    r = rest_request(url_ggl)
+    if not r:
+        log_main("request failed for geocode")
+        return 1
+    
+    data = r.json()
+    log_main.info("latitude: %s, longitude: %s", data['results'][0]['geometry']['location']['lat'],
+                                 data['results'][0]['geometry']['location']['lng'])
+    
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))   
